07429156c5617a9844871e51bab02337ee1889f4
Bogdan Rapaić <bogdan@mainwp.com>
2025-12-29 21:12:10 +0100

    Replace non-breaking spaces with regular spaces in currency format
    Updated the currency formatting logic to use regular spaces instead of HTML non-breaking spaces for 'left_space' and 'right_space' cases. This improves compatibility and display consistency outside of HTML contexts.

--- modules/cost-tracker/classes/class-cost-tracker-utility.php
+++ modules/cost-tracker/classes/class-cost-tracker-utility.php
@@ -214,10 +214,10 @@ class Cost_Tracker_Utility {
                 $format = '%2$s%1$s';
                 break;
             case 'left_space':
-                $format = '%1$s&nbsp;%2$s';
+                $format = '%1$s %2$s';
                 break;
             case 'right_space':
-                $format = '%2$s&nbsp;%1$s';
+                $format = '%2$s %1$s';
                 break;
             default:
                 break;
